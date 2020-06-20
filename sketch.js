b3223a3adef1fbf027c667a1efd32255608aee22
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-06-21 00:01:52 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, gameState,engine, world,dustbin,paper;
 function setup() {
-  createCanvas(400, 400);
-  createSprite(100,100,20,20);
+  createCanvas(800, 400);
+  rectMode(CENTER);
+
+  gameState = "start";
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
 }
 
 function draw() {
-  background(220);
-  drawSprites();
+  if (gameState === "start") {
+    background("black");
+    textSize(20);
+    fill("red");
+    text("This is small game that will teach you the importance of throwing away your trash. \n                 Press Up Arrow to Start, and Up to throw away the trash.", 50, 200)
+    if (keyCode === UP_ARROW) {
+      gameState = "play"
+    }
+  }
+  if (gameState === "play") {
+    rectMode(CENTER);
+    background(0);
+    dustbin.display();
+    paper.display();
+
+  }
+}
+
+
+function keyPressed(){
+  if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 12,
+      y: -13
+    });
+  }
 }
